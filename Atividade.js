273da4c8cd265cb0013ca6e119c048d262db59e9
gustavogcs01 <124313875+gustavogcs01@users.noreply.github.com>
2023-04-05 13:58:06 -0300

    Add files via upload

---
+++ Atividade.js
@@ -0,0 +1,105 @@
+var n1 = parseInt (1);
+var n2 = parseInt (2);
+var soma = n1 + n2;
+console.log(soma)
+
+var n1 = parseInt (5);
+var n2 = parseInt (2);
+var soma = n1 + n2;
+console.log(soma)
+
+var n1 = parseInt (6);
+var n2 = parseInt (8);
+var soma = n1 + n2;
+console.log(soma)
+
+var n1 = parseInt (1);
+var n2 = parseInt (6);
+var soma = n1 + n2;
+console.log(soma)
+
+var n1 = parseInt (7);
+var n2 = parseInt (9);
+var soma = n1 + n2;
+console.log(soma)
+
+/////////////////////////////////////////
+
+var n1 = parseInt (1);
+var n2 = parseInt (2);
+var soma = n1 - n2;
+console.log(soma)
+
+var n1 = parseInt (7);
+var n2 = parseInt (4);
+var soma = n1 - n2;
+console.log(soma)
+
+var n1 = parseInt (8);
+var n2 = parseInt (6);
+var soma = n1 - n2;
+console.log(soma)
+
+var n1 = parseInt (5);
+var n2 = parseInt (4);
+var soma = n1 - n2;
+console.log(soma)
+
+var n1 = parseInt (3);
+var n2 = parseInt (5);
+var soma = n1 - n2;
+console.log(soma)
+
+/////////////////////////////////////////////////////////////
+
+var n1 = parseInt (1);
+var n2 = parseInt (2);
+var soma = n1 * n2;
+console.log(soma)
+
+var n1 = parseInt (6);
+var n2 = parseInt (4);
+var soma = n1 * n2;
+console.log(soma)
+
+var n1 = parseInt (7);
+var n2 = parseInt (3);
+var soma = n1 * n2;
+console.log(soma)
+
+var n1 = parseInt (17);
+var n2 = parseInt (1);
+var soma = n1 * n2;
+console.log(soma)
+
+var n1 = parseInt (8);
+var n2 = parseInt (9);
+var soma = n1 * n2;
+console.log(soma)
+
+//////////////////////////////////////////////////////
+
+var n1 = parseInt (10);
+var n2 = parseInt (2);
+var soma = n1 / n2;
+console.log(soma)
+
+var n1 = parseInt (9);
+var n2 = parseInt (5);
+var soma = n1 / n2;
+console.log(soma)
+
+var n1 = parseInt (3);
+var n2 = parseInt (2);
+var soma = n1 / n2;
+console.log(soma)
+
+var n1 = parseInt (5);
+var n2 = parseInt (4);
+var soma = n1 / n2;
+console.log(soma)
+
+var n1 = parseInt (6);
+var n2 = parseInt (3);
+var soma = n1 / n2;
+console.log(soma)
